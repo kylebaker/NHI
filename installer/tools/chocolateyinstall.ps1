1a5bd0c540d091ded8301cefbf27eb65df8bbf07
kylebaker <kylethomasbaker@gmail.com>
2019-04-17 15:56:30 -0600

    kj

--- installer/tools/chocolateyinstall.ps1
+++ installer/tools/chocolateyinstall.ps1
@@ -11,66 +11,6 @@ $exercises         = "C:\Exercises"
 $pkgPath          = Join-Path $toolsDir "packages.json"
 
 
-# https://stackoverflow.com/questions/28077854/powershell-2-0-convertfrom-json-and-convertto-json-implementation
-function ConvertFrom-Json([object] $item) {
-  Add-Type -Assembly system.web.extensions
-  $ps_js = New-Object system.web.script.serialization.javascriptSerializer
-
-  #The comma operator is the array construction operator in PowerShell
-  return ,$ps_js.DeserializeObject($item)
-}
-
-function LoadPackages {
-  try {
-    $json = Get-Content $pkgPath -ErrorAction Stop
-    $packages = ConvertFrom-Json $json
-  } catch {
-    return $null
-  }
-  return $packages
-}
-
-function InstallOnePackage {
-  param([hashtable] $pkg)
-  $name = $pkg.name
-  $pkgargs = $pkg.args
-  try {
-    $is64Only = $pkg.x64Only
-  } catch {
-    $is64Only = $false
-  }
-
-  if ($is64Only) {
-    if (Get-OSArchitectureWidth -Compare 64) {
-      # pass
-    } else {
-      Write-Warning "[!] Not installing $name on x86 systems"
-      return $true
-    }
-  }
-
-  if ($pkgargs -eq $null) {
-    $args = $globalCinstArgs
-  } else {
-    $args = $pkgargs,$globalCinstArgs -Join " "
-  }
-
-  if ($args -like "*-source*" -Or $args -like "*--package-parameters*" -Or $args -like "*--parameters*") {
-    Write-Warning "[!] Installing using host choco.exe! Errors are ignored. Please check to confirm $name is installed properly"
-    Write-Warning "[!] Executing: iex choco upgrade $name $args"
-    $rc = iex "choco upgrade $name $args"
-    Write-Host $rc
-  } else {
-    choco upgrade $name $args
-  }
-
-  if ($([System.Environment]::ExitCode) -ne 0 -And $([System.Environment]::ExitCode) -ne 3010) {
-    Write-Host "ExitCode: $([System.Environment]::ExitCode)"
-    return $false
-  }
-  return $true
-}
-
 function InitialSetup {
   # Basic system setup
   Update-ExecutionPolicy Unrestricted
@@ -128,30 +68,11 @@ function CleanUp
 
 
 function Main {
-  Write-Host "Before setup"
   InitialSetup
-  Write-Host "After setup, now running MAIN"
 
-  $json = LoadPackages $pkgPath
-  if ($json -eq $null -Or $json.packages -eq $null) {
-    Write-Host "Packages property not found! Exiting"
-    return -1
-  }
+  cinst upgrade sms $globalCinstArgs
+  cinst upgrade wms $globalCinstArgs
 
-  $packages = $json.packages
-  foreach ($pkg in $packages) {
-    $name = $pkg.name
-    Write-Host "Installing" $pkg
-    $rc = InstallOnePackage $pkg
-    if ($rc) {
-      # Try not to get rate-limited
-      if (-Not ($name.Contains(".flare") -or $name.Contains(".fireeye"))) {
-        Start-Sleep -Seconds 5
-      }
-    } else {
-      Write-Error "Failed to install $name"
-    }
-  }
 
   CleanUp
   return 0
